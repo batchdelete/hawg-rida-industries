980b422b6cb6793a5361433a3ac6d52e471f931e
batchdelete <j5g2wmtzpv@privaterelay.appleid.com>
2025-12-26 15:25:15 -0600

    Update hog-rida.js

--- hog-rida.js
+++ hog-rida.js
@@ -1,111 +1,70 @@
+define("ace/theme/tomorrow_night", ["require", "exports", "module", "ace/lib/dom"], function(require, exports, module) {
+  exports.isDark = true;
+  exports.cssClass = "ace-tomorrow_night-theme";
+  exports.cssText = `
+.ace_editor.ace-tomorrow_night-theme {
+  background-image: url('https://media1.tenor.com/m/E1QRYiemnM0AAAAC/hog-rider.gif');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 100% 100%;
+}
 
-define("ace/theme/tomorrow_night", ["require", "exports", "module", "ace/lib/dom"], function(e, t, n) {
-    t.isDark = !0;
-    t.cssClass = "ace-tomorrow_night-theme";
-    t.cssText = `
-      .ace-tomorrow_night-theme .ace_gutter {
-        background: transparent;
-        color: #E2DE16;  
-      }
-      
-      .ace-tomorrow_night-theme .ace_print-margin {
-        width: 1px;
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme {
-        background: url('https://media1.tenor.com/m/E1QRYiemnM0AAAAC/hog-rider.gif') no-repeat center center fixed;
-        background-size: cover;
-        color: #E2DE16;
-        text-shadow: #000000;
-        border: #edebeb;
-      }
-      
-      .ace-tomorrow_night-theme .ace_marker-layer {
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_selection {
-        background-color: #14791B;
-      }
-      
-      .ace-tomorrow_night-theme.ace_multiselect .ace_selection.ace_start {
-        box-shadow: none;
-      }
-      
-      .ace-tomorrow_night-theme .ace_step {
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_bracket {
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_bracket-start {
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_bracket-unmatched {
-        margin: -1px 0 0 -1px;
-        border: none;
-      }
-      
-      .ace-tomorrow_night-theme .ace_active-line {
-        background: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_gutter-active-line {
-        background-color: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_selected-word {
-        border: none;
-        background-color: #14791B;
-      }
-      
-      .ace-tomorrow_night-theme .ace_invisible {
-        color: transparent;
-      }
-      
-      .ace-tomorrow_night-theme .ace_keyword,
-      .ace-tomorrow_night-theme .ace_meta,
-      .ace-tomorrow_night-theme .ace_storage,
-      .ace-tomorrow_night-theme .ace_storage.ace_type {
-        color: #A3A3A3;
-      }
-      
-      .ace-tomorrow_night-theme .ace_keyword.ace_operator {
-        color: #941D1D;
-      }
-      
-      .ace-tomorrow_night-theme .ace_constant.ace_character,
-      .ace-tomorrow_night-theme .ace_constant.ace_language,
-      .ace-tomorrow_night-theme .ace_constant.ace_numeric,
-      .ace-tomorrow_night-theme .ace_keyword.ace_other.ace_unit,
-      .ace-tomorrow_night-theme .ace_support.ace_constant,
-      .ace-tomorrow_night-theme .ace_variable.ace_parameter {
-        color: #E2DE16;
-      }
-      
-      .ace-tomorrow_night-theme .ace_constant.ace_other {
-        color: #E2DE16;
-      }
-      
-      .ace-tomorrow_night-theme .ace_invalid {
-        color: #E2DE16;
-        background-color: transparent;
-      }
-      .ace-tomorrow_night-theme .ace_invalid.ace_deprecated {
-        color: #E2DE16;
-        background-color: transparent;
-      }
-      
-      .ace-tomorrow_night-theme {
-        
-      }
-    `;
-    
-    var r = e("../lib/dom");
-    r.importCssString(t.cssText, t.cssClass);
-  });
-  
+.ace-tomorrow_night-theme .ace_scroller,
+.ace-tomorrow_night-theme .ace_content,
+.ace-tomorrow_night-theme .ace_marker-layer,
+.ace-tomorrow_night-theme .ace_gutter {
+  background: transparent;
+}
+
+.ace-tomorrow_night-theme .ace_gutter {
+  color: #ffffff;
+}
+
+.ace-tomorrow_night-theme .ace_print-margin {
+  width: 1px;
+  background: transparent;
+}
+
+.ace-tomorrow_night-theme .ace_selection {
+  background-color: #14791B;
+}
+
+.ace-tomorrow_night-theme .ace_active-line,
+.ace-tomorrow_night-theme .ace_gutter-active-line {
+  background: transparent;
+}
+
+.ace-tomorrow_night-theme .ace_selected-word {
+  border: none;
+  background-color: #14791B;
+}
+
+.ace_editor.ace-tomorrow_night-theme,
+.ace-tomorrow_night-theme,
+.ace-tomorrow_night-theme .ace_text-layer,
+.ace-tomorrow_night-theme .ace_keyword,
+.ace-tomorrow_night-theme .ace_string,
+.ace-tomorrow_night-theme .ace_constant,
+.ace-tomorrow_night-theme .ace_variable,
+.ace-tomorrow_night-theme .ace_support,
+.ace-tomorrow_night-theme .ace_function,
+.ace-tomorrow_night-theme .ace_type,
+.ace-tomorrow_night-theme .ace_meta,
+.ace-tomorrow_night-theme .ace_comment {
+  color: #ffffff !important;
+}
+
+.ace-tomorrow_night-theme .ace_invisible {
+  color: transparent;
+}
+
+.ace-tomorrow_night-theme .ace_invalid,
+.ace-tomorrow_night-theme .ace_invalid.ace_deprecated {
+  color: #ffffff;
+  background-color: transparent;
+}
+`;
+
+  var dom = require("../lib/dom");
+  dom.importCssString(exports.cssText, exports.cssClass);
+});
